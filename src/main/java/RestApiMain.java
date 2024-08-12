d4b2c8ba58e88eb39aacc737b6c6608f718105c9
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-12 22:52:11 +0530

    constant

--- src/main/java/RestApiMain.java
+++ src/main/java/RestApiMain.java
@@ -1,2 +1,13 @@
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 public class RestApiMain {
+    public static void main(String[] args) throws Exception {
+        //POST REQUEST
+        HttpRequest postRequest = HttpRequest.newBuilder() //this uses the builder pattern so we can call all our request in a chain of patterns
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", Constants.API_KEY);
+                .build();
+    }
 }
