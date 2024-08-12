10c49d337a137527a98d13239b3b5736a9f685a4
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-12 23:32:59 +0530

    gson from json

--- src/main/java/RestApiMain.java
+++ src/main/java/RestApiMain.java
@@ -1,16 +1,50 @@
 
-import java.net.URI;
-import java.net.http.HttpRequest;
+import com.google.gson.Gson;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class RestApiMain {
     public static void main(String[] args) throws Exception {
-     ApiClient apiClient=new ApiClient();
-        //POST REQUEST
-        HttpRequest postRequest = HttpRequest.newBuilder() //this uses the builder pattern so we can call all our request in a chain of patterns
-                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                .header("Authorization", apiClient.makeRequest() )
-                .build()
+        ApiClient apiClient = new ApiClient();
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://github.com/cr7vaibhav/Test_Audio/raw/main/i-like-turtles.mp3");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
 
+        // POST REQUEST
+        HttpRequest postRequest = HttpRequest.newBuilder() // this uses the builder pattern so we can call all our
+                                                           // request in a chain of patterns
+                .uri(new URI("https://api.assemblyai.com/v2/transcript")) // we give it a URL
+                .header("Authorization", apiClient.makeRequest()) // the header with the api key
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest)) // we need to tell it what http request method
+                                                                        // we want to use
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
+
+        System.out.println(postResponse.body());
+
+        
+        // since we need to pass in the id of the request we sent we need to grab it
+        // we use gson
+
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+        System.out.println(transcript.getId());
+
+        // GET REQUEST
+        HttpRequest postRequest = HttpRequest.newBuilder() // this uses the builder pattern so we can call all our
+                                                           // request in a chain of patterns
+                .uri(new URI("https://api.assemblyai.com/v2/transcript")) // we give it a URL
+                .header("Authorization", apiClient.makeRequest()) // the header with the api key
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest)) // we need to tell it what http request method
+                                                                        // we want to use
+                .build();
     }
 }
---
+++ src/main/java/Transcript.java
@@ -0,0 +1,21 @@
+public class Transcript {
+
+    private String audio_url;
+    private String id;
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
