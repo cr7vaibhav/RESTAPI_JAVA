0644da51ad32641b582507520f3d98fafd0e346b
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-12 23:05:48 +0530

    git ignore set

--- src/main/java/RestApiMain.java
+++ src/main/java/RestApiMain.java
@@ -2,12 +2,15 @@
 import java.net.URI;
 import java.net.http.HttpRequest;
 
+
 public class RestApiMain {
     public static void main(String[] args) throws Exception {
+     ApiClient apiClient=new ApiClient();
         //POST REQUEST
         HttpRequest postRequest = HttpRequest.newBuilder() //this uses the builder pattern so we can call all our request in a chain of patterns
                 .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                .header("Authorization", Constants.API_KEY);
-                .build();
+                .header("Authorization", apiClient.makeRequest() )
+                .build()
+
     }
 }
