f0bed22dc5613fdf3905b86d008eca8a51e0fc66
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-12 23:54:24 +0530

    get req

--- src/main/java/RestApiMain.java
+++ src/main/java/RestApiMain.java
@@ -26,11 +26,11 @@ public class RestApiMain {
 
         HttpClient httpClient = HttpClient.newHttpClient();
 
-        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
+        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString()); // this send the post
+                                                                                                   // req
 
         System.out.println(postResponse.body());
 
-        
         // since we need to pass in the id of the request we sent we need to grab it
         // we use gson
 
@@ -39,12 +39,29 @@ public class RestApiMain {
         System.out.println(transcript.getId());
 
         // GET REQUEST
-        HttpRequest postRequest = HttpRequest.newBuilder() // this uses the builder pattern so we can call all our
-                                                           // request in a chain of patterns
-                .uri(new URI("https://api.assemblyai.com/v2/transcript")) // we give it a URL
-                .header("Authorization", apiClient.makeRequest()) // the header with the api key
-                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest)) // we need to tell it what http request method
-                                                                        // we want to use
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript" + transcript.getId()))
+                .header("Authorization", apiClient.makeRequest())
+                // .GET() //doesnt need a request body //since GET is the default we dont need
+                // it
                 .build();
+
+        while (true) {
+            HttpResponse<String> getResponse = httpClient.send(postRequest, BodyHandlers.ofString()); //sends get request
+            transcript = gson.fromJson(postResponse.body(), Transcript.class);
+            System.out.println(transcript.getStatus());
+
+            if("completed".equals(transcript.getStatus())|| "error".equals(transcript.getStatus())){
+                break;
+            }
+            Thread.sleep(1000);//we have to wait atleast a second to send another req
+        }
+
+        //outside this while loop we know the processing has been completed 
+
+
+
+        
+
     }
 }
